8a3a9261e4e64da0841966ede6cbe471ca5dbe8a
Kirill Baranov <kir.astafiew@gmail.com>
2020-07-03 00:21:15 +0400

    121

--- Homework_№1_task_№3.py
+++ Homework_№1_task_№3.py
@@ -3,7 +3,8 @@ a = int(input())
 b = int(input())
 c = int(input())
 D = b ** 2 - 4 * a * c
-radical_of_D = math.sqrt(D)
+d = math.sqrt(D)
 x1 = (- b + d) / 2 * a
 x2 = (-b - d) / 2 * a
-print('x1 = ' + str(x1) + '\n' + 'x2 = ' + str(x2))
+#print('x1 = ' + str(x1) + '\n' + 'x2 = ' + str(x2))
+print(x1)
