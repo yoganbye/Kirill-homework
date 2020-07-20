aa33338fcfb8808848b218a0a2894717606bcb4e
yoganbye <67758770+yoganbye@users.noreply.github.com>
2020-07-20 22:35:08 +0400

    Update hw05_normal.py
    11

--- Part №1/Lesson №5/hw05_normal.py
+++ Part №1/Lesson №5/hw05_normal.py
@@ -113,13 +113,14 @@ try:
         tmp = ''
         maximum = ''
         for i in range(len(a) - 1):
+            if len(tmp) > len(maximum):
+                maximum = tmp
+                tmp = ''
             if len(tmp) == 0 and (a[i] == a[i+1] or a[i] == a[i-1]):
                 tmp += a[i]
             elif (a[i] == a[i+1] or a[i] == a[i-1]) and a[i] in tmp:
                 tmp += a[i]
-            elif len(tmp) > len(maximum):
-                maximum = tmp
-                tmp = ''
+
             else:
                 tmp = ''
     
