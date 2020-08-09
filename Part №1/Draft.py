ae353c892102844e07a3c1e5583255e01edd5c64
Kirill Baranov <kir.astafiew@gmail.com>
2020-08-09 17:56:36 +0400

    09.08 17:56

--- Part №1/Draft.py
+++ Part №1/Draft.py
@@ -1,23 +1,49 @@
+# «Пять безмолвных философов сидят вокруг круглого стола, перед каждым философом стоит 
+# тарелка спагетти. Вилки лежат на столе между каждой парой ближайших философов. 
+# Каждый философ может либо есть, либо размышлять. Прием пищи не ограничен количеством 
+# оставшихся спагетти — подразумевается бесконечный запас. Тем не менее, философ может 
+# есть только тогда, когда держит две вилки — взятую справа и слева (альтернативная 
+# формулировка проблемы подразумевает миски с рисом и палочки для еды вместо тарелок 
+# со спагетти и вилок). Каждый философ может взять ближайшую вилку (если она доступна) 
+# или положить — если он уже держит её. Взятие каждой вилки и возвращение её на стол 
+# являются раздельными действиями, которые должны выполняться одно за другим. Вопрос 
+# задачи заключается в том, чтобы разработать модель поведения (параллельный алгоритм), 
+# при котором ни один из философов не будет голодать, то есть будет вечно чередовать 
+# приём пищи и размышления.»
 import threading
-
 import time
 
 
-def run():
-    i = 1
+class acquire(object):
+    def __init__(self, *locks):
+        self.locks = sorted(locks, key=lambda x: id(x))
 
-    # Бесконечный цикл
+    def __enter__(self):
+        for lock in self.locks:
+            lock.acquire()
+
+    def __exit__(self, ty, val, tb):
+        for lock in reversed(self.locks):
+            lock.release()
+        return False
+
+# The philosopher thread
+def philosopher(left, right):
     while True:
-        print(i)
-        i += 1
+        with acquire(left,right):      
+            time.sleep(0.5)  
+            print(f'Philosopher at {threading.currentThread()} is eating.')
 
-        time.sleep(0.5)
+# The chopsticks
+N_FORKS = 5
+forks = [threading.Lock() for n in range(N_FORKS)]
 
+# Create all of the philosophers
+phils = [threading.Thread(
+    target=philosopher,
+    args=(forks[n], forks[(n + 1) % N_FORKS])
+) for n in range(N_FORKS)]
 
-if __name__ == '__main__':
-    thread = threading.Thread(target=run)
-    thread.daemon = True
-    thread.start()
-    thread.join(4)
-
-    print('Quit!')
+# Run all of the philosophers
+for p in phils:
+    p.start()
