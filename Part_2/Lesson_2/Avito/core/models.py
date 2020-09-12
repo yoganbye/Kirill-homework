a16ee90d5d2b6a0ae9ba23625097613d9ca2ffc1
Kirill Baranov <kir.astafiew@gmail.com>
2020-09-12 19:48:08 +0400

    12.09

--- Part_2/Lesson_2/Avito/core/models.py
+++ Part_2/Lesson_2/Avito/core/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
-class Profile(models.Model):
+def avatar_path(instance, filename):
+    return 'user_{0}/avatars/{1}'.format(instance.user.id, filename)
+
+def ad_path(instance, filename):
+    return 'user_{0}/posts/{1}'.format(instance.author.id, filename)
+
+    
+class Profile(models.Model): 
     """
     Модель профиля
     """
@@ -11,8 +18,11 @@ class Profile(models.Model):
         User, on_delete=models.CASCADE, related_name='user_profile'
     )
     birth_date = models.DateField('Date of birth', null=True, blank=True)
-    avatar = models.ImageField(upload_to='/avatar', default=None)
+    avatar = models.ImageField(upload_to=avatar_path, default=None)
     friends = models.ManyToManyField(User, related_name='friends', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class CategoriesAd(models.Model):
@@ -20,7 +30,9 @@ class CategoriesAd(models.Model):
     Модель категорий объявлений
     '''
     name = models.CharField(max_length=30)
-   
+
+    def __str__(self):
+        return self.name 
 
 
 class Ad(models.Model):
@@ -31,8 +43,11 @@ class Ad(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ForeignKey(CategoriesAd, on_delete=models.CASCADE)
     description = models.TextField(max_length=1000, blank=True)
-    image = models.ImageField(upload_to='/image')
+    image = models.ImageField(upload_to=ad_path, default=None)
     date_pub = models.DateTimeField(default=timezone.now)
     date_edit = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return 'Author {}, date {}'.format(self.author.username, self.date_pub) 
 
 
