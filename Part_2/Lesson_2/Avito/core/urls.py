f6235d4bef615531bd9fdb0285a82cf8d7de104a
Kirill Baranov <kir.astafiew@gmail.com>
2020-09-13 17:35:25 +0400

    13.09

--- Part_2/Lesson_2/Avito/core/urls.py
+++ Part_2/Lesson_2/Avito/core/urls.py
@@ -5,4 +5,10 @@ from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('announcement/', views.announce, name='announce'),
+    path('announcement/<int:announce_id>/', views.det_announce, name='det_announce'),
+    path('announcement/<int:announce_id>/create/', views.ad_create, name='ad_create'),
+    path('announcement/<int:announce_id>/edit/', views.ad_edit, name='ad_edit'),
+    path('announcement/<int:announce_id>/delete/', views.ad_delete, name='ad_delete'),
+    path('categories/', views.categories, name='categories'),
 ]
--- Part_2/Lesson_2/Avito/core/views.py
+++ Part_2/Lesson_2/Avito/core/views.py
@@ -3,4 +3,32 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Доска объявлений Авито')
+    return HttpResponse('Главная страница')
+
+
+def announce(request):
+    return HttpResponse('Все объявления')
+
+
+def det_announce(request, announce_id):
+    response = 'Детальное представление объявления №{}'.format(announce_id)
+    return HttpResponse(response)
+
+
+def categories(request):
+    return HttpResponse('Страница с разделением по категориям')
+
+
+def ad_edit(request, announce_id):
+    response = 'Редактирование объявления №{}'.format(announce_id)
+    return HttpResponse(response)
+
+
+def ad_create(request, announce_id):
+    response = 'Создание объявления №{}'.format(announce_id)
+    return HttpResponse(response)
+
+
+def ad_delete(request, announce_id):
+    response = 'Удаление объявления объявления №{}'.format(announce_id)
+    return HttpResponse(response)
